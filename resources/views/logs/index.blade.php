1e1b62fb32728b6cbfc67ce750e1705d272e4c80
Josemar Pascoal <josemarpas26@gmail.com>
2026-05-15 23:39:43 +0200

    TESTE

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -13,6 +13,8 @@
 
 @section('content')
 
+
+
 {{-- Filtros --}}
 <x-card title="Filtros" icon="fas fa-filter" class="mb-6">
     <form method="GET" class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
